61c537a9bf41ef3f03e79bdfdf77a9bbbaa7588e
Drew Paul <drew@Drews-MBP.fios-router.home>
2020-03-15 23:31:38 -0400

    Added signup form on frontend

--- frontend/gui/src/routes.js
+++ frontend/gui/src/routes.js
@@ -3,13 +3,15 @@ import { Route } from 'react-router-dom';
 
 import FoodList from './containers/FoodListView';
 import FoodDetail from './containers/FoodDetailView';
-import Login from './containers/Login'
+import Login from './containers/Login';
+import Signup from './containers/Signup';
 
 const BaseRouter = () => (
     <div>
         <Route exact path='/' component={FoodList} />
         <Route exact path='/foods/:foodID/' component={FoodDetail} />
         <Route exact path='/login/' component={Login} />
+        <Route exact path='/signup/' component={Signup} />
     </div>
 );
 
