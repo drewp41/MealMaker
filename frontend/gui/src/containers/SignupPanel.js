1fd6bb87429f4ed0602ec2a318c87aa76c580838
Drew Paul <arpman41@gmail.com>
2020-05-25 00:57:01 -0400

    Made footer responsive to mobile

--- frontend/gui/src/containers/SignupPanel.js
+++ frontend/gui/src/containers/SignupPanel.js
@@ -11,10 +11,14 @@ function SignupPanel(props) {
     return (
         <div className='signupPanel'>
             <div className='signupPanelBody'>
+                <div className='signupPanelFillerLR' />
                 <div className='signupPanelReadyText'>
+                    {/* It tasts better when it's effortless. */}
+                    {/* It does everything but make the sandwich. */}
                     <span style={{ color: '#40a66e' }}>Ready to get your diet on track?</span>
                     Join Meal Maker today.
                 </div>
+                <div className='signupPanelFillerM' />
                 <div className='signupPanelInput'>
                     <Input className='signupPanelEmail' placeholder='Email' size='large' />
                     <a className='genButton' onClick={() => console.log('hi')}
@@ -22,6 +26,7 @@ function SignupPanel(props) {
                         SIGN UP
                     </a>
                 </div>
+                <div className='signupPanelFillerLR' />
             </div>
         </div>
     )
