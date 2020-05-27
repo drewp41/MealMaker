d2d1277fa78e5fdb39681b32eeabb1b6179c4a3c
Drew Paul <arpman41@gmail.com>
2020-05-26 22:51:16 -0400

    Made the info panel responsive

--- frontend/gui/src/containers/InfoPanel.js
+++ frontend/gui/src/containers/InfoPanel.js
@@ -16,36 +16,39 @@ import wrench from '../wrench.svg';
 
 function InfoPanel(props) {
     return (
-        <div className='infoPanel'>
-            <div className='infoSquare'>
-                <img className='infoSquareIcon' src={calendar} alt="calendar" draggable='false'
-                    style={{ width: 48, height: 48, marginBottom: '13px' }} />
-                <h3 className='infoSquareTitle'>Remove your anxiety</h3>
-                <p className='infoSquareText'>
-                    Meal Maker will generate all your meals ahead of time.
-                    That way, you don’t have to worry about what you’re doing to eat for the next meal.
+        <>
+            <div className='infoPanel'>
+                <div className='infoSquare'>
+                    <img className='infoSquareIcon' src={calendar} alt="calendar" draggable='false'
+                        style={{ width: 48, height: 48, marginBottom: '13px' }} />
+                    <h3 className='infoSquareTitle'>Remove your anxiety</h3>
+                    <p className='infoSquareText'>
+                        Meal Maker will generate all your meals ahead of time.
+                        That way, you don’t have to worry about what you’re doing to eat for the next meal.
                 </p>
-            </div>
-            <div className='infoSquare'>
-                <img className='infoSquareIcon' src={wrench} alt="calendar" draggable='false'
-                    style={{ width: 42, height: 42, marginBottom: '19px' }} />
-                <h3 className='infoSquareTitle'>Customize to your needs</h3>
-                <p className='infoSquareText'>
-                    You can customize your preferences to fit your needs.
-                    Whether you liking snacking throughout the day,
-                    or need to hit your protein intake, Meal Maker has you covered.
+                </div>
+                <div className='infoSquare'>
+                    <img className='infoSquareIcon' src={wrench} alt="calendar" draggable='false'
+                        style={{ width: 42, height: 42, marginBottom: '19px' }} />
+                    <h3 className='infoSquareTitle'>Customize to your needs</h3>
+                    <p className='infoSquareText'>
+                        You can customize your preferences to fit your needs.
+                        Whether you liking snacking throughout the day,
+                        or need to hit your protein intake, Meal Maker has you covered.
                 </p>
-            </div>
-            <div className='infoSquare'>
-                <img className='infoSquareIcon' src={save} alt="calendar" draggable='false'
-                    style={{ width: 41, height: 41, marginBottom: '20px' }} />
-                <h3 className='infoSquareTitle'>Save your favorite meals</h3>
-                <p className='infoSquareText'>
-                    See a meal that peaks your interest?  Use your Meal Maker account to save it for later,
-                    or pin it in place to keep it in your future meal plans.
+                </div>
+                <div className='infoSquare'>
+                    <img className='infoSquareIcon' src={save} alt="calendar" draggable='false'
+                        style={{ width: 41, height: 41, marginBottom: '20px' }} />
+                    <h3 className='infoSquareTitle'>Save your favorite meals</h3>
+                    <p className='infoSquareText'>
+                        See a meal that peaks your interest?  Use your Meal Maker account to save it for later,
+                        or pin it in place to keep it in your future meal plans.
                 </p>
+                </div>
             </div>
-        </div>
+            <div className='infoPanelSpacer' />
+        </>
     )
 }
 
