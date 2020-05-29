0cf687c2e2a4b87b79612d604b9b653507bd893f
Drew Paul <arpman41@gmail.com>
2020-05-29 17:32:53 -0400

    Added links to footer and added functionality to the email button

--- frontend/gui/src/containers/Footer.js
+++ frontend/gui/src/containers/Footer.js
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+
+import { Alert } from 'antd';
 
 import {
     GithubOutlined, LinkedinFilled,
-    MailOutlined,
+    MailOutlined, CheckCircleOutlined
 } from '@ant-design/icons';
 
 import whiteLogo from '../whiteLogo.svg';
 
+function copyToClipboard(text) {
+    var dummy = document.createElement("textarea");
+    // to avoid breaking orgain page when copying more words
+    // cant copy when adding below this code
+    // dummy.style.display = 'none'
+    document.body.appendChild(dummy);
+    //Be careful if you use texarea. setAttribute('value', value), which works with "input" does not work with "textarea". – Eduard
+    dummy.value = text;
+    dummy.select();
+    document.execCommand("copy");
+    document.body.removeChild(dummy);
+}
+
 function Footer(props) {
+
+    const [displayEmail, setDisplayEmail] = useState(false);
+
     return (
         <div className='footer'>
             {/* background: rgb(27,28,29) and dividers: rgb(50,51,52) */}
@@ -21,24 +39,52 @@ function Footer(props) {
                 </div>
 
                 <div className='footerLinks'>
-                    <a href='#'>Home</a>
-                    <Link to='/howitworks/#'>
-                        <a href='#'>How it works</a>
+                    <Link to='/'>
+                        Home
                     </Link>
-                    <a href='#'>About</a>
+                    <Link to='/howitworks'>
+                        How it works
+                    </Link>
+                    <Link to='/about'>
+                        About
+                    </Link>
                     <a href='#'>Feedback</a>
                     <a href='https://github.com/drewp41/MealMaker'>Source code</a>
                 </div>
                 <div className='footerSocials'>
-                    <a href='https://github.com/drewp41'> <GithubOutlined style={{ fontSize: '26px' }} /> </a>
-                    <a href='https://www.linkedin.com/in/andrew-paul-5a5036178/'> <LinkedinFilled style={{ fontSize: '26px' }} /> </a>
-                    <a href='#'> <MailOutlined style={{ fontSize: '26px' }} /> </a>
+                    <a href='https://github.com/drewp41'>
+                        <GithubOutlined style={{ fontSize: '26px' }} />
+                    </a>
+                    <a href='https://www.linkedin.com/in/andrew-paul-5a5036178/'>
+                        <LinkedinFilled style={{ fontSize: '26px' }} />
+                    </a>
+                    <span className='footerEmail' onClick={() => setDisplayEmail(!displayEmail)}>
+                        <MailOutlined style={{ fontSize: '26px' }} />
+                    </span>
                 </div>
+                {displayEmail &&
+                    <div className='footerEmailText'>
+                        {copyToClipboard('drewpaul4141@gmail.com')}
+                        <CheckCircleOutlined />&nbsp; Copied to clipboard
+                        <div className='space8' />
+                        drewpaul4141@gmail.com
+                    </div>
+                }
+                {/* {displayEmail &&
+                    <Alert
+                        message="Copied to clipboard"
+                        description="drewpaul4141@gmail.com"
+                        type="success"
+                        showIcon
+                        closable
+                        onClose={() => setDisplayEmail(false)}
+                    />
+                } */}
                 <div className='footerCopyright'>
                     © 2020 Andrew Paul
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 
