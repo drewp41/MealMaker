168081534db8b4562f106d5694e0c16a0b13dee4
Drew Paul <arpman41@gmail.com>
2020-06-06 11:53:18 -0400

    Added user field to Django model, and filtered the views to only show the current user's meals

--- backend/src/foods/api/views.py
+++ backend/src/foods/api/views.py
@@ -3,13 +3,20 @@ from rest_framework import viewsets
 from foods.models import Food
 from .serializers import FoodSerializer
 
+
 # this class handles all of the requests below (get, post, update, delete)
 # in much less code
 
-
 class FoodViewSet(viewsets.ModelViewSet):
     serializer_class = FoodSerializer
-    queryset = Food.objects.all()
+    # queryset = Food.objects.all()
+    # queryset = Food.objects.filter(userID=2)
+
+    def get_queryset(self):
+        return Food.objects.filter(created_by=self.request.user.username)
+        # return Food.objects.filter(created_by='drew')
+        # return Food.objects.all()
+
 
 # from rest_framework.generics import (
 #     ListAPIView,
--- backend/src/foods/models.py
+++ backend/src/foods/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 
 
 class Food(models.Model):
@@ -6,6 +8,8 @@ class Food(models.Model):
     # carb = models.PositiveSmallIntegerField(default=0, blank=True)
     # protein = models.PositiveSmallIntegerField(default=0, blank=True)
     # fat = models.PositiveSmallIntegerField(default=0, blank=True)
+    # user = models.ForeignKey(get_user_model, on_delete=models.CASCADE)
+    created_by = models.CharField(max_length=100)
     meal = models.TextField(default='')
 
     def __str__(self):
